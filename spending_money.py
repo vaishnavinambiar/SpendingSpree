1c20b5f88795c146bb937ef664675ef58b1febfa
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-02-05 17:50:59 +0530

    Update spending_money.py

--- spending_money.py
+++ spending_money.py
@@ -1,51 +1,56 @@
 default = 1000
 notify = raw_input("You have " + str(default)+ " money to spend")
-user = float(raw_input("Enter the cost of the item."))
-choice = raw_input("Are you sure you want to spent?")
 
 def spendings_one(default):
     """  """
+    user = float(raw_input("Enter the cost of the item."))
+    choice = raw_input("Are you sure you want to spent?")
+    balance = default - user
     if choice == str('y'):
-        return ("You have $990.01 remaining")
+        return ("You have "+ str(balance)+" remaining")
     elif choice == str('n'):
-        return default
+        return ("No spendings")
 
-def spendings_two(default):
+def spendings_two(balance):
     """ """
     user_one = float(raw_input("Enter the cost of the item."))
     choice_one = raw_input("Are you sure you want to spent?")
+    balance_one = balance - user_one
     if choice_one== str('y'):
-        return ("You have $446.78 remaining")
+        return("You have "+str(balance_one)+" remaining")
     elif choice_one == str('n'):
-        return ("You have $990.01 remaining")
+        return ("You have "+str(balance)+" remaining")
     
        
-def spendings_three(default):
+def spendings_three(balance_one):
     """ """
     user_two = float(raw_input("Enter the cost of the item."))
     choice_two = raw_input("Are you sure you want to spent?")
+    balance_two = balance_one - user_two
     if choice_two == str('y'):
-        return ("You have $757.36 remaining")
+        return  ("You have "+str(balance_two)+" remaining")
     elif choice_two == str('n'):
-        return ("You have $446.78  remaining")
+        return ("You have "+str(balance_one)+"  remaining")
 
-def spendings_four(default):
+def spendings_four(balance_two):
     """ """
     user_three = float(raw_input("Enter the cost of the item."))
     choice_three = raw_input("Are you sure you want to spent?")
+    balance_three = balance_two- user_three
     if choice_three == str('y'):
-        return ("You have $57.36 remaining")
+        return ("You have" +str(balance_three)+" remaining")
     elif choice_three == str('n'):
-        return ("You have $446.78  remaining")
+        return ("You have "+str(balance_two)+"  remaining")
 
-def spendings_five(default):
+def spendings_five(balance_three):
     """ """
     user_four = float(raw_input("Enter the cost of the item."))
     choice_four = raw_input("Are you sure you want to spent?")
+    balance_four = balance_three - user_four
     if choice_four == str('y'):
-        return ("You dont have enough money to purchase and now owe :$731.87")
+        return ("You dont have enough money to purchase and now owe :"+str(balance_four))
     elif choice_four== str('n'):
-        return ("You have $446.78  remaining")
+        return ("You have "+str(balance_three)+"  remaining")
 
 
 
@@ -55,6 +60,7 @@ if __name__ == "__main__":
     print spendings_three(default)
     print spendings_four(default)
     print spendings_five(default)
+
 
 
 
